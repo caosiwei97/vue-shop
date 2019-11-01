192108e8c238bb990b158333b058486487b4eb76
caosiwei <1286612323@qq.com>
2019-11-01 22:40:33 +0800

    feat: 🎸 add footer-guide component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <footer-guide></footer-guide>
   </div>
 </template>
 
 <script>
+import FooterGuide from '@/components/FooterGuide/FooterGuide.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: { FooterGuide }
 }
 </script>
 
