3074a721fea58f8ed6898e9847207d3dd1b770c8
caosiwei <1286612323@qq.com>
2019-11-05 12:40:25 +0800

    feat: 🎸 FooterGuide compone done

--- src/components/FooterGuide/FooterGuide.vue
+++ src/components/FooterGuide/FooterGuide.vue
@@ -1,14 +1,71 @@
 <template>
   <div class="footer-guide">
-    FooterNav
+    <router-link class="guide-item" :to="item.routerLink" v-for="(item, index) in routerDataLists" :key="index">
+      <div class="item-icon">
+        <i :class="['iconfont', item.iconClass]"></i>
+      </div>
+      <div class="item-title">{{ item.iconTitle }}</div>
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'FooterGuide'
+  name: 'FooterGuide',
+  data () {
+    return {
+      routerDataLists: [
+        {
+          routerLink: { name: '外卖' },
+          iconClass: 'iconwaimai-',
+          iconTitle: '外卖'
+        },
+        {
+          routerLink: { name: '搜索' },
+          iconClass: 'iconsousuo',
+          iconTitle: '搜索'
+        },
+        {
+          routerLink: { name: '订单' },
+          iconClass: 'icondingdan1',
+          iconTitle: '订单'
+        },
+        {
+          routerLink: { name: '我的' },
+          iconClass: 'icongeren1',
+          iconTitle: '我的'
+        }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped lang="stylus">
+$borderColor = #e4e4e4
+$iconColor = #999
+.router-link-active
+  color $yellow !important
+.footer-guide
+  top-border-1px($borderColor)
+  position fixed
+  z-index 100
+  left 0
+  right 0
+  bottom 0
+  height px2rem(50)
+  display flex
+  .guide-item
+    display flex
+    flex 1
+    text-align center
+    flex-direction column
+    align-items center
+    margin px2rem(5)
+    color $iconColor
+    & > div
+      font-size .75em
+      margin-top px2rem(2)
+      .iconfont
+        font-size 2em
 </style>
