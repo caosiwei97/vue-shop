177e8a91f460dd202c655189037cc24d03746101
caosiwei <1286612323@qq.com>
2019-11-05 22:45:44 +0800

    feat: 🎸 HomeSwiper component done

---
+++ src/views/Home/components/HomeSwiper.vue
@@ -0,0 +1,92 @@
+<template>
+  <nav class="home-swiper">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <!-- 总页数 -->
+      <swiper-slide v-for="(page, index) in pages" :key="index">
+        <!-- 每页的图标导航 -->
+        <div class="swiper-icon-wrapper" v-for="(item, index) in page" :key="index">
+          <div class="swiper-icon">
+            <img :src="item.iconUrl" :alt="item.iconTitle" />
+          </div>
+          <p class="swiper-title">{{ item.iconTitle }}</p>
+        </div>
+      </swiper-slide>
+      <!-- 分页圆点 -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </nav>
+</template>
+
+<script>
+import 'Swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+  name: 'HomeSwiper',
+  props: {
+    iconLists: Array
+  },
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      }
+    }
+  },
+  computed: {
+    pages () {
+      // 分页逻辑 如果 当前页的icon个数大于8就分页，使用二维数据
+      const pages = []
+      this.iconLists.forEach((item, index) => {
+        const page = Math.floor(index / 8) // 页数
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+
+      return pages
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.home-swiper >>> .swiper-pagination-bullet-active
+  background-color $yellow
+.home-swiper >>> .swiper-wrapper
+  height 0
+  padding-bottom 50%
+  .swiper-icon-wrapper
+    overflow hidden
+    float left
+    position relative
+    width 25%
+    height 0
+    padding-bottom 23%
+    .swiper-icon
+      position absolute
+      left 0
+      right 0
+      top 0
+      margin-bottom px2rem(10)
+      & > img
+        width px2rem(50)
+        display block
+        margin 0 auto
+    .swiper-title
+      position absolute
+      left 0
+      right 0
+      bottom px2rem(15)
+      text-align center
+      font-size .8125em
+      color $navTextColor
+      ellipsis()
+</style>
