f08839aaad544cc4e2d6e1d812c2bec11550579a
caosiwei <1286612323@qq.com>
2019-11-09 12:09:50 +0800

    feat: 🎸 static Order component done

--- src/views/Order/Order.vue
+++ src/views/Order/Order.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="order">
     <header-top :header-title="title"></header-top>
+    <div class="order-loginout">
+      <img src="./images/person.png" alt="">
+      <h3>登陆后查看外卖清单</h3>
+      <button>立即登陆</button>
+    </div>
   </div>
 </template>
 
@@ -17,4 +22,29 @@ export default {
 }
 </script>
 
-<style scoped lang="stylus"></style>
+<style scoped lang="stylus">
+.order
+  overflow hidden
+  .order-loginout
+    width 60%
+    padding-top px2rem(130)
+    text-align center
+    margin 0 auto
+    & > img
+      width 100%
+      height 30%
+      display block
+    & > h3
+      padding px2rem(10) 0
+      font-size 1em
+      color #6a6a6a
+    & > button
+      display inline-block
+      background $yellow
+      font-size .875em
+      outline none
+      padding px2rem(10) px2rem(20)
+      border 0
+      color $white
+      border-radius px2rem(4)
+</style>
