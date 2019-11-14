1cf3d616e61b9cb546fbad39fb1edb6af5513537
caosiwei <1286612323@qq.com>
2019-11-14 21:56:26 +0800

    HomeSwiper done

---
+++ src/views/Home/components/HomeSwiper.vue
@@ -0,0 +1,102 @@
+<template>
+  <nav class="home-swiper">
+    <swiper :options="swiperOption" ref="mySwiper">
+      <!-- 总页数 -->
+      <swiper-slide v-for="(page, index) in pages" :key="index">
+        <!-- 每页的图标导航 -->
+        <a
+          class="swiper-icon-wrapper"
+          v-for="(item, index) in page"
+          :key="index"
+          href="#"
+        >
+          <div class="swiper-icon">
+            <img :src="item.iconUrl" :alt="item.iconTitle" />
+          </div>
+          <p class="swiper-title">{{ item.iconTitle }}</p>
+        </a>
+      </swiper-slide>
+      <!-- 分页圆点 -->
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </nav>
+</template>
+
+<script>
+import 'swiper/dist/css/swiper.css'
+import { swiper, swiperSlide } from 'vue-awesome-swiper'
+
+export default {
+  name: 'HomeSwiper',
+  props: {
+    iconLists: Array
+  },
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data() {
+    return {
+      swiperOption: {
+        pagination: {
+          el: '.swiper-pagination'
+        }
+      }
+    }
+  },
+  computed: {
+    pages() {
+      // 分页逻辑 如果 当前页的icon个数大于8就分页，使用二维数据
+      const pages = []
+      this.iconLists.forEach((item, index) => {
+        const page = Math.floor(index / 8) // 页数
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+
+      return pages
+    }
+  }
+}
+</script>
+
+<style scoped lang="stylus">
+.home-swiper >>> .swiper-pagination-bullet-active
+  background-color $yellow
+.home-swiper >>> .swiper-wrapper
+  height 0
+  padding-bottom 50%
+.home-swiper
+  background-color $white
+  bottom-border-1px($borderColor)
+  .swiper-icon-wrapper
+    overflow hidden
+    float left
+    width 25%
+    height 0
+    padding-bottom 25%
+    position relative
+    .swiper-icon
+      margin-bottom px2rem(10)
+      img
+        width px2rem(50)
+        display block
+        margin 0 auto
+    .swiper-title
+      text-align center
+      font-size .8em
+      color $navTextColor
+      ellipsis()
+@media only screen and (min-width 768px)
+  .swiper-icon
+  .swiper-title
+    position absolute
+    left 50%
+    transform translateX(-50%)
+  .swiper-title
+    bottom 50%
+  .swiper-icon
+    bottom 60%
+</style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <!-- header slot -->
     <header-slot :header-title="headerTile">
       <!-- left slot -->
       <template #left>
@@ -10,20 +11,73 @@
         <span class="top-right-login slot-right">登陆|注册</span>
       </template>
     </header-slot>
+    <!-- swiper nav -->
+    <home-swiper :icon-lists="iconLists"></home-swiper>
   </div>
 </template>
 
 <script>
 import HeaderSlot from 'components/HeaderSlot'
+import HomeSwiper from './components/HomeSwiper'
+
+// icon img
+import iconOne from 'images/home/1.jpg'
+import iconTwo from 'images/home/2.jpg'
+import iconThree from 'images/home/3.jpg'
+import iconFour from 'images/home/4.jpg'
+import iconFive from 'images/home/5.jpg'
+import iconSix from 'images/home/6.jpg'
+import iconSeven from 'images/home/7.jpg'
+import iconEight from 'images/home/8.jpg'
+import iconNight from 'images/home/9.jpg'
 
 export default {
   name: 'Home',
   components: {
-    HeaderSlot
+    HeaderSlot,
+    HomeSwiper
   },
   data() {
     return {
-      headerTile: '金石滩街道大连民族大学4号公寓'
+      headerTile: '金石滩街道大连民族大学4号公寓',
+      iconLists: [
+        {
+          iconUrl: iconOne,
+          iconTitle: '甜品饮品'
+        },
+        {
+          iconUrl: iconTwo,
+          iconTitle: '甜品饮品'
+        },
+        {
+          iconUrl: iconThree,
+          iconTitle: '甜品饮品'
+        },
+        {
+          iconUrl: iconFour,
+          iconTitle: '甜品饮品'
+        },
+        {
+          iconUrl: iconFive,
+          iconTitle: '甜品饮品'
+        },
+        {
+          iconUrl: iconSix,
+          iconTitle: '甜品饮品'
+        },
+        {
+          iconUrl: iconSeven,
+          iconTitle: '甜品饮品'
+        },
+        {
+          iconUrl: iconEight,
+          iconTitle: '甜品饮品'
+        },
+        {
+          iconUrl: iconNight,
+          iconTitle: '甜品饮品'
+        }
+      ]
     }
   }
 }
