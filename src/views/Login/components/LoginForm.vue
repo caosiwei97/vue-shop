424eafafcc6eb2f969c8eb813930e33b22de4493
caosiwei <1286612323@qq.com>
2019-11-16 11:11:13 +0800

    LoginForm done

---
+++ src/views/Login/components/LoginForm.vue
@@ -0,0 +1,116 @@
+<template>
+  <form action="#" class="login-form">
+    <!-- phone -->
+    <section class="form-phone">
+      <input type="tel" :placeholder="placeholder" maxlength="11" />
+      <button disabled="disabled" v-show="!isPasswordWay">获取验证码</button>
+    </section>
+    <!-- password -->
+    <section class="form-password" v-show="isPasswordWay">
+      <input type="tel" maxlength="8" placeholder="密码" />
+      <base-switch></base-switch>
+    </section>
+    <!-- verification -->
+    <section class="form-verification">
+      <input type="tel" maxlength="8" placeholder="验证码" />
+      <img src="" alt="" v-show="isPasswordWay" />
+    </section>
+    <!-- protocol -->
+    <section class="form-protocol" v-show="!isPasswordWay">
+      <p>
+        温馨提示：未注册文哲外卖帐号的手机号，登录时将自动注册，且代表已同意
+        <a href="#">《用户服务协议》</a>
+      </p>
+    </section>
+    <button type="submit" class="form-submit">登陆</button>
+  </form>
+</template>
+
+<script>
+import BaseSwitch from 'components/BaseSwitch'
+
+export default {
+  name: 'LoginForm',
+  props: {
+    isPasswordWay: Boolean
+  },
+  components: {
+    BaseSwitch
+  },
+  data() {
+    return {
+      isActive: false
+    }
+  },
+  computed: {
+    placeholder() {
+      if (this.isPasswordWay) {
+        return '手机/邮箱/用户名'
+      }
+      return '手机'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+$textColor = #333
+$btnColor = #ccc
+$inputBorder = #ddd
+
+.active
+
+.login-form
+  overflow hidden
+  .form-phone
+  .form-verification
+  .form-password
+    margin-top px2rem(16)
+    height px2rem(48)
+    font-size .875em
+    background-color $white
+    position relative
+    >input
+      width 100%
+      height 100%
+      border-radius px2rem(4)
+      padding-left px2rem(10)
+      border px2rem(1) solid $inputBorder
+      box-sizing border-box
+      outline 0
+      &:focus
+        border 1px solid $themeColor
+    >button
+      position absolute
+      top 50%
+      right px2rem(10)
+      transform translateY(-50%)
+      border none
+      background transparent
+      color $btnColor
+  .form-password
+    .base-switch
+      position absolute
+      top 50%
+      right 5%
+      transform translateY(-50%)
+  .form-protocol
+    margin-top px2rem(12)
+    color $iconColor
+    text-align left
+    line-height 1.4
+    font-size .875em
+    a
+      color $themeColor
+  .form-submit
+    display block
+    width 100%
+    height px2rem(42)
+    line-height 2.625
+    margin-top px2rem(30)
+    border-radius px2rem(4)
+    background $themeColor
+    color $white
+    border 0
+    font-size 1em
+</style>
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,13 +1,109 @@
 <template>
   <div class="login">
-    login
+    <!-- icon link -->
+    <router-link :to="{ name: '外卖' }" class="login-link">
+      <i class="iconfont iconicon_arrow_left"></i>
+    </router-link>
+    <div class="login-wrapper">
+      <!-- header title -->
+      <div class="login-header">
+        <h2 class="header-title">文哲外卖</h2>
+        <div class="header-content">
+          <a
+            href="#"
+            v-for="(item, index) in loginWays"
+            :key="index"
+            @click="handleClick(item.wayClass)"
+            :class="{ active: isActive(item.wayClass) }"
+          >
+            {{ item.title }}
+          </a>
+        </div>
+      </div>
+      <!-- form -->
+      <login-form :isPasswordWay="isPasswordWay"></login-form>
+      <p class="login-footer">关于我们</p>
+    </div>
   </div>
 </template>
 
 <script>
+import LoginForm from './components/LoginForm'
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  components: {
+    LoginForm
+  },
+  data() {
+    return {
+      currentActiveClass: 'message',
+      loginWays: [
+        {
+          wayClass: 'message',
+          title: '短信登陆'
+        },
+        {
+          wayClass: 'password',
+          title: '密码登陆'
+        }
+      ]
+    }
+  },
+  computed: {
+    isPasswordWay() {
+      return this.currentActiveClass === 'password'
+    }
+  },
+  methods: {
+    handleClick(way) {
+      this.currentActiveClass = way
+    },
+    isActive(way) {
+      return this.currentActiveClass === way
+    }
+  }
 }
 </script>
 
-<style lang="stylus" scoped></style>
+<style scoped lang="stylus">
+.active
+  color $themeColor !important
+  font-weight 700
+  border-bottom px2rem(2) solid $themeColor
+  box-sizing border-box
+.login
+  overflow hidden
+  background-color $white
+  height 100%
+  .login-link
+    width px2rem(30)
+    height px2rem(30)
+    display inline-block
+    text-align center
+    line-height 1.875
+    .iconfont
+      font-size 1.25em
+  .login-wrapper
+    width 80%
+    margin 0 auto
+    padding-top px2rem(60)
+    text-align center
+    .login-header
+      overflow hidden
+      .header-title
+        font-size 2.5em
+        color $themeColor
+      .header-content
+        padding px2rem(40) px2rem(70) 0
+        font-size .875em
+        display flex
+        justify-content space-between
+        a
+          color $textColor
+          padding-bottom px2rem(4)
+    .login-footer
+      color $iconColor
+      margin-top px2rem(20)
+      font-size .75em
+</style>
