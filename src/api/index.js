9c9e455fc968f4410eae37d29d01af1bad40bed8
caosiwei <1286612323@qq.com>
2019-11-26 20:46:12 +0800

    定义接口请求

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,146 @@
  * api接口的统一出口
  */
 
-import request from '@uils/request'
+import axios from '@/utils/request'
+
+// 统一url管理
+const url = {
+  getLocationUrl: '/position',
+  getShoplistsByLocationUrl: '/shops',
+  getShopCategoryUrl: '/index_category',
+  getShoplistsBySearchUrl: '/search_shops',
+  getMessageCodeUrl: '/sendcode',
+  getUserInfoUrl: '/userinfo',
+  getUserLogoutUrl: '/logout',
+  getCaptchaUrl: '/captcha',
+  postUserLoginByPasswordUrl: '/login_pwd',
+  postUserLoginByMessageCodeUrl: '/login_sms'
+}
+
+/**
+ * 根据经纬度获取位置详情
+ *
+ * @param {String} geohash 经纬度
+ * @returns {Promise}
+ */
+function getLocationApi(geohash) {
+  return axios.get(`${url.getLocationUrl}/${geohash}`)
+}
+
+/**
+ * 根据经纬度获取商铺列表
+ *
+ * @param {Object} { latitude, longitude }
+ * @returns {Promise}
+ */
+function getShoplistsByLocationApi({ latitude, longitude }) {
+  return axios.get(url.getShoplistsByLocationUrl, {
+    params: {
+      latitude,
+      longitude
+    }
+  })
+}
+
+/**
+ * 根据关键字搜索商家列表
+ *
+ * @param {Object} { keywords, geohash }
+ * @returns {Promise}
+ */
+function getShoplistsBySearchApi({ keywords, geohash }) {
+  return axios.get(url.getShoplistsBySearchUrl, {
+    params: {
+      keywords,
+      geohash
+    }
+  })
+}
+
+/**
+ * 获取食品分类列表
+ *
+ * @returns {Promise}
+ */
+function getShopCategoryApi() {
+  return axios.get(url.getShopCategoryUrl)
+}
+
+/**
+ * 请求手机验证码
+ *
+ * @param {Number} phone
+ * @returns {Promise}
+ */
+function getMessageCodeApi(phone) {
+  return axios.get(`${url.getMessageCodeUrl}/${phone}`)
+}
+
+/**
+ * 根据会话获取用户信息
+ *
+ * @returns {Promise}
+ */
+function getUserInfoApi() {
+  return axios.get(url.getUserInfoUrl)
+}
+
+/**
+ * 获取一次性验证码
+ *
+ * @returns {Promise}
+ */
+function getCaptchaApi() {
+  return axios.get(url.getCaptchaUrl)
+}
+
+/**
+ * 用户退出登录
+ *
+ * @returns {Promise}
+ */
+function getUserLogoutApi() {
+  return axios.get(url.getUserLogoutUrl)
+}
+
+/**
+ *  用户使用密码登陆
+ *
+ * @param {Object} { name, pwd, captcha }
+ * @returns {Promise}
+ */
+function postUserLoginByPasswordApi({ name, pwd, captcha }) {
+  return axios.post(url.postUserLoginByPasswordUrl, {
+    params: {
+      name,
+      pwd,
+      captcha
+    }
+  })
+}
+
+/**
+ * 手机号验证码登陆
+ *
+ * @param {*} { phone, messageCode }
+ * @returns {Promise}
+ */
+function postUserLoginByMessageCodeApi({ phone, messageCode }) {
+  return axios.post(url.postUserLoginByMessageCodeUrl, {
+    phone,
+    messageCode
+  })
+}
+
 export default {
-  request
+  getLocationApi,
+  getShoplistsByLocationApi,
+  getShopCategoryApi,
+  getShoplistsBySearchApi,
+  getMessageCodeApi,
+  getUserInfoApi,
+  getCaptchaApi,
+  getUserLogoutApi,
+  postUserLoginByPasswordApi,
+  postUserLoginByMessageCodeApi
 }
--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import api from '@/api'
 import 'styles/css/reset.css'
 import 'styles/css/layout.css'
 import 'styles/css/iconfont.css'
 
+Vue.prototype.$axios = api // 将所有api请求挂在Vue原型中
 Vue.config.productionTip = false
 
 new Vue({
