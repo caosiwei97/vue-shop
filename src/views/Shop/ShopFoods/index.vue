85688c2c9baa9b711fd9b689b8b31ce459627db7
caosiwei <1286612323@qq.com>
2019-12-08 15:29:00 +0800

    滑动逻辑完成

--- src/views/Shop/ShopFoods/index.vue
+++ src/views/Shop/ShopFoods/index.vue
@@ -2,8 +2,16 @@
   <div class="shop-foods">
     <!-- 左栏分类 -->
     <div class="foods-category">
-      <ul class="category-list">
-        <li class="list-item" v-for="(item, index) in foodLists" :key="index">
+      <ul class="category-list" ref="category">
+        <li
+          :class="[
+            'list-item',
+            { 'list-item-current': currentIndex === index }
+          ]"
+          v-for="(item, index) in foodLists"
+          :key="index"
+          @click="handleCategoryClick(index)"
+        >
           <div class="item-detail">
             <img :src="item.icon" v-if="item.icon" />
             {{ item.name }}
@@ -14,7 +22,7 @@
     <!-- 右栏食物分类详情 -->
     <div class="foods-detail">
       <!-- 每个分类 -->
-      <ul class="detail-category">
+      <ul class="detail-category" ref="detail">
         <li
           class="category-block"
           v-for="(category, index) in foodLists"
@@ -61,12 +69,25 @@ export default {
   data() {
     return {
       scrollY: 0, // 右边滑动距离
-      tops: [] // 右边初始化每个食物分类顶部li的坐标距离
+      detailTops: [], // 右边每个食物分类顶部li的坐标距离
+      categoryTops: [] // 左边每个食物分类顶部li的坐标距离
     }
   },
   computed: {
-    ...mapState(['foodLists'])
+    ...mapState(['foodLists']),
+    currentIndex() {
+      let index = 0
+      index = this.detailTops.findIndex((ele, index, args) => {
+        return this.scrollY >= ele && this.scrollY < args[index + 1]
+      })
+      return index
+    }
   },
+  // watch: {
+  //   currentIndex(index) {
+  //     this.categoryScroll.scrollTo(0, -this.categoryTops[index])
+  //   }
+  // },
   mounted() {
     // 请求数据后才初始化Bscroll
     this.$store.dispatch('getShopLists', () => {
@@ -78,10 +99,42 @@ export default {
   },
   methods: {
     _initCategoryScroll() {
-      new BScroll('.foods-category')
+      // 初始化Bscroll对象
+      this.categoryScroll = new BScroll('.foods-category', {
+        click: true
+      })
+      this.categoryTops = this._getClientHeight('category')
     },
     _initDetailScroll() {
-      new BScroll('.foods-detail')
+      // 初始化Bscroll对象
+      this.detailScroll = new BScroll('.foods-detail', {
+        probeType: 3,
+        scrollY: true
+      })
+      // 监听滑动事件,实时获取滑动距离
+      this.detailScroll.on('scroll', ({ y }) => {
+        this.scrollY = Math.abs(y)
+      })
+      this.detailTops = this._getClientHeight('detail')
+    },
+    // 获取每个分类li元素的初始高度数组
+    _getClientHeight(ele) {
+      const eleLists = [...this.$refs[ele].children]
+
+      let tempArr = []
+      let top = 0
+
+      tempArr.push(top) // 将第一个li元素的距离放入数组
+      eleLists.forEach(item => {
+        top += item.clientHeight
+        tempArr.push(top)
+      })
+
+      return tempArr
+    },
+    handleCategoryClick(index) {
+      // 点击左边分类，右边滑到目标位置
+      this.detailScroll.scrollTo(0, -this.detailTops[index], 300)
     }
   }
 }
@@ -102,9 +155,10 @@ export default {
     background $backgroundColor
     .category-list
       .list-item
+        width 100%
         height px2rem(50)
         display table
-        margin 0 auto
+        text-align center
         .item-detail
           display table-cell
           font-size .8em
@@ -114,6 +168,9 @@ export default {
           >img
             vertical-align middle
             width px2rem(12)
+      .list-item-current
+        background $white
+        color $themeColor
   .foods-detail
     flex 4
     .detail-category
@@ -165,4 +222,5 @@ export default {
               >del
                 padding-left px2rem(5)
                 color $iconColor
+                font-size .8em
 </style>
