938f87ba05c09bbdba8b24ec90d0da02de57adec
caosiwei <1286612323@qq.com>
2019-12-12 21:30:06 +0800

    使用date-fns替代moment

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-import moment from 'moment' // 日期格式化
+// import moment from 'moment' // 日期格式化
+import format from 'date-fns/format' // 代替moment 优化打包
 
 //自定义过滤器
-Vue.filter('dateFormat', function(value, formatStr = 'YYYY-MM-DD HH:mm:ss') {
-  return moment(value).format(formatStr)
+Vue.filter('dateFormat', function(value, formatStr) {
+  return format(value, formatStr || 'yyyy-mm-dd hh:mm:ss')
 })
